0ab1db7d37c9b3d5d9556eb3a7afcae51039ac8d
jathom30 <jathom30@gmail.com>
2022-02-26 10:47:12 -0500

    update currency formatting

--- src/helpers/currency.ts
+++ src/helpers/currency.ts
@@ -1,6 +1,9 @@
 export const toCurrency = (val: number) => {
-  if (val > 1) return Math.round(val * 100) / 100
-  return val
+  if (val > 1) {
+    const roundedVal = Math.round(val * 100) / 100
+    return roundedVal.toFixed(2).split( /(?=(?:\d{3})+(?:\.|$))/g ).join( "," )
+  }
+  return Math.round(val * 100000) / 100000
 }
 
 export const getExchangedAmount = (val: number, exchangeRate: number) => val * exchangeRate
