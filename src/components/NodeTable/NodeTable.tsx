daceb5a566d8bde55871b7afe1666138e0ed160c
jathom30 <jathom30@gmail.com>
2022-02-27 10:55:11 -0500

    add ability to show days or date in compound table

--- src/components/NodeTable/NodeTable.tsx
+++ src/components/NodeTable/NodeTable.tsx
@@ -26,6 +26,7 @@ const earningsPeriods = [
 
 export const NodeTable: React.FC<{id: string}> = ({id}) => {
   const [activeEarningsPeriod, setActiveEarningsPeriod] = useState(1)
+  const [compoundDisplay, setCompoundDisplay] = useState<'Days' | 'Date'>('Days')
   const nodecount = useRecoilValue(nodeCount(id))
   const nodecost = useRecoilValue(nodeCost(id))
   const dailyReward = useRecoilValue(nodeRewards(id))
@@ -43,7 +44,23 @@ export const NodeTable: React.FC<{id: string}> = ({id}) => {
             <h3>Node Compound Table (after node compound tax, sales tax, and monthly fee)</h3>
             <GridBox gridTemplateColumns={`repeat(4, 1fr)`} gap="0.5rem" alignItems="flex-end" paddingTop="0.25rem">
               <span>Nodes</span>
-              <span>Days to compound</span>
+              <div>
+                <FlexBox>
+                  <button
+                    className={`NodeTable__earnings-btn ${compoundDisplay === 'Days' ? 'NodeTable__earnings-btn--active' : ''}`}
+                    onClick={() => setCompoundDisplay('Days')}
+                  >
+                    Days
+                  </button>
+                  <button
+                    className={`NodeTable__earnings-btn ${compoundDisplay === 'Date' ? 'NodeTable__earnings-btn--active' : ''}`}
+                    onClick={() => setCompoundDisplay('Date')}
+                  >
+                    Date
+                  </button>
+                </FlexBox>
+                <span>to compound</span>
+              </div>
               <span>{tokenReward ? token?.symbol.toUpperCase() : '%'}/day</span>
               <div>
                 <FlexBox>
@@ -63,14 +80,20 @@ export const NodeTable: React.FC<{id: string}> = ({id}) => {
           </FlexBox>
         </div>
         {data?.map((row) => (
-          <TableRow key={row.nodeCount} row={row} id={id} earningsPeriod={activeEarningsPeriod} />
+          <TableRow key={row.nodeCount} row={row} id={id} earningsPeriod={activeEarningsPeriod} compoundDisplay={compoundDisplay} />
         ))}
       </FlexBox>
     </div>
   )
 }
 
-const TableRow: React.FC<{row: CompoundData, id: string, earningsPeriod: number}> = ({row, id, earningsPeriod}) => {
+function addDays(date: Date, days: number) {
+  var result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
+}
+
+const TableRow: React.FC<{row: CompoundData, id: string, earningsPeriod: number; compoundDisplay: 'Date' | 'Days'}> = ({row, id, earningsPeriod, compoundDisplay}) => {
   const nodecount = useRecoilValue(nodeCount(id))
   const dailyEarnings = useRecoilValue(dailyNodeEarnings({id, taxType: 'compound'}))
   const tokenReward = useRecoilValue(tokenRewardAtom(id))
@@ -81,11 +104,14 @@ const TableRow: React.FC<{row: CompoundData, id: string, earningsPeriod: number}
     const daily = dailyEarnings * numberOfNodes / nodecount
     return toCurrency(daily * earningsPeriod * (exchange?.value || 1))
   }
+  const today = new Date()
+  const date = addDays(today, row.day).toLocaleDateString()
+  const dayRow = compoundDisplay === 'Days' ? Math.round(row.day).toLocaleString() : date
   return (
     <div className="NodeTable__row">
       <GridBox gridTemplateColumns={`repeat(4, 1fr)`} gap="0.5rem">
         <span>{row.nodeCount}</span>
-        <span>{Math.round(row.day).toLocaleString()}</span>
+        <span>{dayRow}</span>
         <span>{Math.round((row.rewards + Number.EPSILON) * 1000) / 1000}{!tokenReward && '%'}</span>
         <span style={{whiteSpace: 'nowrap'}}>{exchange?.unit} {getDailyEarnings(row.nodeCount)}</span>
       </GridBox>
