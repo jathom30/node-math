41af259f836023ba49c87a0d84c039436d375d23
jathom30 <jathom30@gmail.com>
2022-04-02 09:00:10 -0400

    remove ad

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { FlexBox } from 'components';
 import banner from 'assets/aj_thomas_site_logo.png'
-import desktopAd from 'assets/nodeWhale.png'
-import mobileAd from 'assets/nodeWhaleWide.png'
+// import desktopAd from 'assets/nodeWhale.png'
+// import mobileAd from 'assets/nodeWhaleWide.png'
 import ReactGA from 'react-ga4'
 import './Footer.scss'
 
@@ -16,17 +16,6 @@ export const Footer = ({isMobile}: {isMobile: boolean}) => {
   return (
     <div className="Footer">
       <FlexBox flexDirection='column' gap=".5rem">
-        <a className='Footer__ad-link' href="https://nodewhales.com/" target="_blank" rel="noreferrer">
-        {!isMobile ? (
-            <>
-              <img className='Footer__ad' src={desktopAd} alt="node whale ad" />
-              <img className='Footer__ad' src={desktopAd} alt="node whale ad" />
-              <img className='Footer__ad' src={desktopAd} alt="node whale ad" />
-            </>
-        ) : (
-          <img className='Footer__ad' src={mobileAd} alt="node whale ad" />
-          )}
-          </a>
         <FlexBox gap='1rem' alignItems="center" justifyContent="space-between">
           <span className='Footer__text'><strong>These are not guaranteed rewards.</strong> Please do your own research and invest at your own risk. This is not financial advice.</span>
           <a onClick={handleAdClick} href="https://www.youtube.com/channel/UC73GDN0hsd1DpIEUjpOjoww" target="_blank" rel="noreferrer">
@@ -52,3 +41,19 @@ const YoutubeLogo = () => (
     <path style={{fill:'red', fillOpacity:1, fillRule:'nonzero', stroke:'none'}} d="M149.938 79.223s-63.22 0-79.086 4.123c-8.496 2.374-15.492 9.37-17.866 17.99-4.123 15.867-4.123 48.727-4.123 48.727s0 32.984 4.123 48.601c2.374 8.62 9.245 15.491 17.866 17.865 15.992 4.248 79.085 4.248 79.085 4.248s63.345 0 79.211-4.123c8.621-2.374 15.492-9.12 17.74-17.865 4.249-15.742 4.249-48.601 4.249-48.601s.125-32.985-4.248-48.852c-2.25-8.62-9.12-15.491-17.74-17.74-15.867-4.373-79.212-4.373-79.212-4.373zm-20.116 40.48 52.6 30.36-52.6 30.234v-60.594z"/>
   </svg>
 )
+
+// const FooterAd = ({isMobile}: {isMobile: boolean}) => {
+//   return (
+//     <a className='Footer__ad-link' href="https://nodewhales.com/" target="_blank" rel="noreferrer">
+//       {!isMobile ? (
+//           <>
+//             <img className='Footer__ad' src={desktopAd} alt="node whale ad" />
+//             <img className='Footer__ad' src={desktopAd} alt="node whale ad" />
+//             <img className='Footer__ad' src={desktopAd} alt="node whale ad" />
+//           </>
+//       ) : (
+//         <img className='Footer__ad' src={mobileAd} alt="node whale ad" />
+//       )}
+//     </a>
+//   )
+// }
